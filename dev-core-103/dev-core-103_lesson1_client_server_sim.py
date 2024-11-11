87122339dc900210c182bb7c941968a2cd96bc69
Madina <turarova.madina.2001@gmail.com>
2024-11-11 13:37:10 +0500

    lesson1 and lesson2

---
+++ dev-core-103/dev-core-103_lesson1_client_server_sim.py
@@ -0,0 +1,9 @@
+def client_request():
+  print("Клиент отправляет запрос на сервер.")
+
+def server_response():
+  print("Сервер обрабатывает запрос.")
+  print("Сервер отправляет ответ: Добро пожаловать в интернет!")
+
+client_request()
+server_response()
