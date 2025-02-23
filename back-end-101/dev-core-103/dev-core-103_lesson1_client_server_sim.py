74d05219792558ab558d0c1f5fe230a8e597e893
Madina <turarova.madina.2001@gmail.com>
2025-02-23 21:05:55 +0500

    fix files

--- back-end-101/dev-core-103/dev-core-103_lesson1_client_server_sim.py
+++
@@ -1,9 +0,0 @@
-def client_request():
-  print("Клиент отправляет запрос на сервер.")
-
-def server_response():
-  print("Сервер обрабатывает запрос.")
-  print("Сервер отправляет ответ: Добро пожаловать в интернет!")
-
-client_request()
-server_response()
--- back-end-101/dev-ops-101/task-5/system_info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Дата и время: $(date)"
-echo "Пользователь: $(whoami)"
-
-file_count=$(ls -1 | wc -l)
-echo "Количество файлов в текущей директории: $file_count"
